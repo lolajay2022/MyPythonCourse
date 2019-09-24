131eaf2078440a76b0b78c1f40d1d592a52fcca4
Lola Jay <susan.a.grossman@gmail.com>
2019-09-24 09:24:10 -0700

    v2 space issue

--- Ch03/basic.py
+++ Ch03/basic.py
@@ -1,5 +1,7 @@
-print('hello')
-
+import sys
+# print('hello')
+print('hello and welcome to a basic python program')
 print('welcome')
 food=input("what is your favorite food?")
-print("you like" + food +"? I might like it too")
+print("you like " + food +" ? I might like it too!")
+print('Hello, ' + sys.argv[1]+ 'and welcome to basic python program')
